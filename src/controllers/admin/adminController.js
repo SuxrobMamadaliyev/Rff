e94c535157e134302ecf5e587269a6fe1f82104b
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-06-24 12:53:55 +0500

    Update adminController.js

--- src/controllers/admin/adminController.js
+++ src/controllers/admin/adminController.js
@@ -1,31 +1,22 @@
 // -----------------------------------------------------------------------------
-// Admin panel entry: opening / closing the panel and entering admin scenes.
+// Admin panel entry.
 // -----------------------------------------------------------------------------
 import { adminMenuKeyboard } from '../../keyboards/adminKeyboards.js';
 import { mainMenuKeyboard } from '../../keyboards/userKeyboards.js';
 import { SCENES } from '../../utils/constants.js';
 
-/**
- * Open the admin panel (reply keyboard).
- * @param {import('telegraf').Context} ctx
- */
 export const openPanel = (ctx) =>
   ctx.reply('🛠 <b>Admin panel</b>\n\nKerakli amalni tanlang 👇', {
     parse_mode: 'HTML',
     ...adminMenuKeyboard(),
   });
 
-/**
- * Leave the admin panel and return to the user menu.
- * @param {import('telegraf').Context} ctx
- */
 export const exitPanel = (ctx) =>
   ctx.reply('⬅️ Asosiy menyuga qaytdingiz.', {
     parse_mode: 'HTML',
-    ...mainMenuKeyboard(),
+    ...mainMenuKeyboard(true), // admin uchun, shuning uchun true
   });
 
-// Each admin keyboard button simply enters the relevant wizard scene.
 export const enterAddBalance = (ctx) => ctx.scene.enter(SCENES.ADMIN_ADD_BALANCE);
 export const enterSubBalance = (ctx) => ctx.scene.enter(SCENES.ADMIN_SUB_BALANCE);
 export const enterBonus = (ctx) => ctx.scene.enter(SCENES.ADMIN_BONUS);
@@ -33,3 +24,4 @@ export const enterBroadcast = (ctx) => ctx.scene.enter(SCENES.ADMIN_BROADCAST);
 export const enterForward = (ctx) => ctx.scene.enter(SCENES.ADMIN_FORWARD);
 export const enterBan = (ctx) => ctx.scene.enter(SCENES.ADMIN_BAN);
 export const enterUnban = (ctx) => ctx.scene.enter(SCENES.ADMIN_UNBAN);
+
