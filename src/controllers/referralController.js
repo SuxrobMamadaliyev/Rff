56ef4cde8fdbf740790e35288eb7fbd619caa29d
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-06-26 10:18:46 +0500

    Update referralController.js

--- src/controllers/referralController.js
+++ src/controllers/referralController.js
@@ -1,6 +1,7 @@
 // -----------------------------------------------------------------------------
 // Referral view: shows the user's referral link and statistics.
 // -----------------------------------------------------------------------------
+import { Markup } from 'telegraf';
 import { buildReferralLink } from '../utils/helpers.js';
 import messages from '../utils/messages.js';
 
@@ -11,10 +12,17 @@ import messages from '../utils/messages.js';
 export const showReferral = (ctx) => {
   const user = ctx.state.user;
   const link = buildReferralLink(user.telegramId);
+  const shareText = "Bu botda men orqali ro'yxatdan o'tsang, sen ham bonus olasan! 🎁";
+  const shareUrl = `https://t.me/share/url?url=${encodeURIComponent(link)}&text=${encodeURIComponent(shareText)}`;
+
   return ctx.reply(messages.referral(user, link), {
     parse_mode: 'HTML',
     disable_web_page_preview: true,
+    ...Markup.inlineKeyboard([
+      Markup.button.url('📤 Ulashish', shareUrl),
+    ]),
   });
 };
 
 export default showReferral;
+
